fe90eb22c47dfb17b2e0e8c7f02aa45578ca3175
Vinícius Silva <vinisilva45@live.com>
2020-09-07 17:08:16 -0300

    Adicionar arquivos de projeto.

---
+++ Pedidos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pedidos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
